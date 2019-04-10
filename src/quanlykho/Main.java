3da2ef264de31ea738f48d6a3f2803b6151381c6
congnghe1234 <n16dccn150@student.ptithcm.edu.vn>
2019-04-10 10:44:36 +0700

    QL

---
+++ src/quanlykho/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quanlykho;
+
+/**
+ *
+ * @author admin
+ */
+public class Main {
+    public static String taikhoan;
+    public static void main(String[] args) {
+        Jframe_DN j=new Jframe_DN();
+        j.setVisible(true);
+    }
+}
